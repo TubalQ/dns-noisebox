44933a5d3e0136ff1a3dbd38c99ab5449a890250
Prox3r <71977914+TubalQ@users.noreply.github.com>
2025-08-08 10:49:00 +0000

    Update noisebox.sh

--- noisebox.sh
+++ noisebox.sh
@@ -1,81 +1,102 @@
 #!/bin/bash
 
-# DNS resolvers to query (e.g., Cloudflare, Technitium, Quad9)
+# DNS resolver to query (ex.Cloudflare or google)
 RESOLVERS=("9.9.9.9" "1.1.1.1" "8.8.8.8" "94.140.14.14" "208.67.222.222")
 
-# Number of queries per run
-NUM_QUERIES=40
+# number of queries/run
+NUM_QUERIES=15
 
-# Wordlist for generating random domains
-WORDLIST="/opt/noicebox/words.txt"
+# Wordlist for random domains
+WORDLIST="/root/noise_box/words.txt"
+# List of TLDs to randomize
+TLDS=("com" "net" "org" "info" "biz" "io" "co" "app" "xyz" "se" "nu" "it" "ai" "me" "dev")
 
 # Log file
 LOGFILE="/var/log/dns_noise.log"
-MAXSIZE=50000  # Max size in bytes (~50 KB)
+MAXSIZE=50000  # Max storlek i byte (~50 KB)
 
-# Rotate log file if it exceeds max size
+# Log rotation
 if [[ -f "$LOGFILE" && $(stat -c%s "$LOGFILE") -gt $MAXSIZE ]]; then
   mv "$LOGFILE" "$LOGFILE.1"
   touch "$LOGFILE"
 fi
 
-# List of real popular domains
+
+# List of real domains
 REAL_DOMAINS=(
-  # Italian domains
-  "google.it" "youtube.com" "facebook.com" "instagram.com" "amazon.it" "repubblica.it" "corriere.it" "gazzetta.it"
-  "ansa.it" "larepubblica.it" "lastampa.it" "ilsole24ore.com" "mediaworld.it" "esselunga.it" "coop.it" "eprice.it"
-  "subito.it" "immobiliare.it" "tripadvisor.it" "zalando.it" "ebay.it" "penna.it" "sky.it" "rai.it" "spotify.com"
-  "netflix.it" "tinder.com" "bakeca.it" "ilfattoquotidiano.it" "fanpage.it" "tgcom24.mediaset.it" "tg24.sky.it"
-  "rai.it" "mediaset.it" "calcio.com" "mondadori.it" "unieuro.it" "libreriauniversitaria.it" "mondadoristore.it"
-  "mondo.it" "ilgusto.it" "ristorante.it" "directory.it" "virgo.it" "terremoto.it" "idea.it" "blogspot.it"
-  "wordpress.com" "blog.it" "panoramio.com" "iltempo.it" "milanotoday.it" "romaoggi.it" "torinoggi.it"
-  "napolifree.it" "firenzeweb.it" "forlìtoday.it" "bolognatoday.it" "palermotoday.it" "genovatoday.it"
-  "toscana.it" "duedipuglia.it" "ilcentro.it" "abruzzo.it" "ilmessaggero.it" "tg1.it" "cronaca.it" "calcio24.it"
-  "cialdashop.it" "idee.it" "donnaoggi.it" "avis.it" "omegle.it" "universitaly.it" "registro.it"
-  "ministerodellauniversita.it" "anime.it" "videomedicina.it" "salute.it" "farmacia.it" "territorio.it"
+  # Put your custom domains here
+
+  # Italian Domains
+  "google.it" "youtube.com" "facebook.com" "instagram.com" "amazon.it" "repubblica.it" "corriere.it" "gazzetta.it" \
+  "ansa.it" "larepubblica.it" "lastampa.it" "ilsole24ore.com" "mediaworld.it" "esselunga.it" "coop.it" "eprice.it" \
+  "subito.it" "immobiliare.it" "tripadvisor.it" "zalando.it" "ebay.it" "penna.it" "sky.it" "rai.it" "spotify.com" \
+  "netflix.it" "tinder.com" "bakeca.it" "ilfattoquotidiano.it" "fanpage.it" "tgcom24.mediaset.it" "tg24.sky.it" \
+  "rai.it" "mediaset.it" "calcio.com" "mondadori.it" "unieuro.it" "libreriauniversitaria.it" "mondadoristore.it" \
+  "mondo.it" "ilgusto.it" "ristorante.it" "directory.it" "virgo.it" "terremoto.it" "idea.it" "blogspot.it" \
+  "wordpress.com" "blog.it" "panoramio.com" "iltempo.it" "milanotoday.it" "romaoggi.it" "torinoggi.it" \
+  "napolifree.it" "firenzeweb.it" "forlìtoday.it" "bolognatoday.it" "palermotoday.it" "genovatoday.it" \
+  "toscana.it" "duedipuglia.it" "ilcentro.it" "abruzzo.it" "ilmessaggero.it" "tg1.it" "cronaca.it" "calcio24.it" \
+  "cialdashop.it" "idee.it" "donnaoggi.it" "avis.it" "omegle.it" "universitaly.it" "registro.it" \
+  "ministerodellauniversita.it" "anime.it" "videomedicina.it" "salute.it" "farmacia.it" "territorio.it" \
   "comuni-italiani.it" "ailm.com" "mio.it" "studiareinitalia.it" "consob.it" "torino.it"
 
   # English / American domains
-  "google.com" "youtube.com" "facebook.com" "instagram.com" "reddit.com" "amazon.com" "wikipedia.org" "nytimes.com"
-  "cnn.com" "foxnews.com" "nbcnews.com" "abcnews.go.com" "washingtonpost.com" "latimes.com" "hulu.com"
-  "netflix.com" "disneyplus.com" "hbo.com" "spotify.com" "apple.com" "microsoft.com" "espn.com" "weather.com"
-  "accuweather.com" "usatoday.com" "forbes.com" "businessinsider.com" "techcrunch.com" "cnet.com" "pcmag.com"
-  "wired.com" "theverge.com" "mashable.com" "lifehacker.com" "medium.com" "buzzfeed.com" "vice.com" "rollingstone.com"
-  "people.com" "npr.org" "pbs.org" "time.com" "politico.com" "theatlantic.com" "axios.com" "vox.com"
-  "independent.co.uk" "bloomberg.com" "reuters.com" "gatech.edu" "stanford.edu" "mit.edu" "harvard.edu"
-  "cbsnews.com" "techtarget.com" "quora.com" "stackoverflow.com" "github.com" "bitbucket.org" "linkedin.com"
-  "indeed.com" "monster.com" "glassdoor.com" "airbnb.com" "booking.com" "trulia.com" "zillow.com"
-  "realtor.com" "craigslist.org" "hackernews.com" "privacytools.io" "duckduckgo.com" "startpage.com" "qwant.com"
+  "google.com" "youtube.com" "facebook.com" "instagram.com" "reddit.com" "amazon.com" "wikipedia.org" "nytimes.com" \
+  "cnn.com" "foxnews.com" "nbcnews.com" "abcnews.go.com" "washingtonpost.com" "latimes.com" "hulu.com" \
+  "netflix.com" "disneyplus.com" "hbo.com" "spotify.com" "apple.com" "microsoft.com" "espn.com" "weather.com" \
+  "accuweather.com" "usatoday.com" "forbes.com" "businessinsider.com" "techcrunch.com" "cnet.com" "pcmag.com" \
+  "wired.com" "theverge.com" "mashable.com" "lifehacker.com" "medium.com" "buzzfeed.com" "vice.com" "rollingstone.com" \
+  "people.com" "npr.org" "pbs.org" "time.com" "politico.com" "theatlantic.com" "axios.com" "vox.com" \
+  "independent.co.uk" "bloomberg.com" "reuters.com" "gatech.edu" "stanford.edu" "mit.edu" "harvard.edu" \
+  "cbsnews.com" "techtarget.com" "quora.com" "stackoverflow.com" "github.com" "bitbucket.org" "linkedin.com" \
+  "indeed.com" "monster.com" "glassdoor.com" "airbnb.com" "booking.com" "trulia.com" "zillow.com" \
+  "realtor.com" "craigslist.org" "hackernews.com" "privacytools.io" "duckduckgo.com" "startpage.com" "qwant.com" \
   "tiktok.com"
-)
+) 
 
-# Create log file if it doesn't exist
+
+# Create log if not existing 
 touch "$LOGFILE"
 chmod 600 "$LOGFILE"
 
-# Perform DNS queries
+# Run queries
 for i in $(seq 1 $NUM_QUERIES); do
   TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
 
   if (( RANDOM % 10 < 7 )); then
+    # Jabberish-Domains
     WORD=$(shuf -n 1 "$WORDLIST" | tr -d "'")
-    DOMAIN="${WORD}.xyz"
+    TLD=$(shuf -n 1 -e "${TLDS[@]}")
+    DOMAIN="${WORD}.${TLD}"
     TYPE="NONSENSE"
   else
-    DOMAIN=$(shuf -n 1 -e "${REAL_DOMAINS[@]}")
+    # Real domains
+    BASE_DOMAIN=$(shuf -n 1 -e "${REAL_DOMAINS[@]}")
+    if (( RANDOM % 4 == 0 )); then
+      SUB=$(shuf -n 1 -e "${SUBDOMAINS[@]}")
+      DOMAIN="${SUB}.${BASE_DOMAIN}"
+    else
+      DOMAIN="$BASE_DOMAIN"
+    fi
     TYPE="REAL"
   fi
 
-  # Select a random resolver
   RESOLVER=$(shuf -n 1 -e "${RESOLVERS[@]}")
-
-  # Query using dig with timeout and retry limit
   dig @"$RESOLVER" "$DOMAIN" +timeout=2 +tries=1 +short > /dev/null
-
-  # Write to log
   echo "$TIMESTAMP [$TYPE] ($RESOLVER) $DOMAIN" >> "$LOGFILE"
-
-  # Random delay between queries (0.5–2.5 seconds)
   sleep $(awk -v min=0.5 -v max=2.5 'BEGIN{srand(); print min+rand()*(max-min)}')
 done
 
+
+  # Choose a random resolver
+  RESOLVER=$(shuf -n 1 -e "${RESOLVERS[@]}")
+
+  # hit via selected resolver
+  dig @"$RESOLVER" "$DOMAIN" +timeout=2 +tries=1 +short > /dev/null
+
+  # log
+  echo "$TIMESTAMP [$TYPE] ($RESOLVER) $DOMAIN" >> "$LOGFILE"
+
+  # randomize delay
+  sleep $(awk -v min=0.5 -v max=2.5 'BEGIN{srand(); print min+rand()*(max-min)}')
+done
